c1c01f91d82e18ad2e5f37b9a134f52ecb288057
SheYan <shelley.yang@softwire.com>
2025-07-10 10:13:02 +0100

    some nonworking history page things

--- src/history.tsx
+++ src/history.tsx
@@ -1,6 +1,44 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {json} from "node:stream/consumers";
+
+class history{
+    public id: number;
+    public name: string;
+    public content: string;
+    constructor(id: number, name: string, content: string) {
+        this.id = id;
+        this.name = name;
+        this.content = content;
+    }
+}
 
 function History(): React.ReactElement {
-    return <></>
+    const [content, setContent] = useState<history[]>([]);
+
+    useEffect(() =>{
+        fetch ('/history.json')
+        .then((response: Response) => response.json())
+            .then (content => setContent(content))
+            .catch((error: Error) => console.error('cannot fetch content', error));
+    }, []);
+
+    console.log(content);
+    if (!content) {
+        return <div>Loading content...</div>;
+    }
+
+    function populateContent(){
+        if (content != undefined){
+            let body = document.createElement("div");
+            body.innerHTML = content[0].name;
+            document.getElementById('content-container')?.appendChild(body);
+        }
+    }
+
+    return <>
+    <h1> History of TfL Buses </h1>
+    <div className="navigation"></div>
+    <div id="content-container"></div>{populateContent()}
+    </>
 }
 export default History;
