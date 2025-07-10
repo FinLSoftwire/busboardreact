d3008b4ae670b7d2b47ad844d26582da2e38ce93
SheYan <shelley.yang@softwire.com>
2025-07-10 17:32:52 +0100

    alignment weird but navbar ok

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -7,9 +7,9 @@ const Navbar = () => {
     return (<>
     <Nav className="navbar">
         <div className="container-fluid">
-            <img src="./bus.png" className="icon"></img>
-                <div className = 'nav-item'><a href="/">Home</a></div>
-                <div className = 'nav-item'><a href="/history">History</a></div>
+            <img src="./bus.gif" className="icon"></img>
+            <div className = 'nav-item'><a href="/">Home</a></div>
+            <div className = 'nav-item'><a href="/history">History</a></div>
         </div>
     </Nav>
         </>
